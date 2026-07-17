85e41eb86cab42dfa995a878c58cfa24cd25b58c
Adam Wulf <adam@adamwulf.me>
2026-07-17 14:28:22 -0500

    Add PerceptronDemo UIKit iPad app scaffold
    Xcode UIKit template for the iPad port of the Mark I Perceptron
    Simulator. Starting point for porting the main instrument-panel UI.
    
    Also removes local Claude settings from the repo.

---
+++ PerceptronDemo/PerceptronDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PerceptronDemo
+//
+//  Created by Adam Wulf on 7/17/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PerceptronDemo/PerceptronDemoTests/PerceptronDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PerceptronDemoTests.swift
+//  PerceptronDemoTests
+//
+//  Created by Adam Wulf on 7/17/26.
+//
+
+import Testing
+@testable import PerceptronDemo
+
+struct PerceptronDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
